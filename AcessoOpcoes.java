566693d0aa23ace20be8c7843e1c2ccd5b1b6ed9
Antonio Marcos Sousa de Oliveira <150481966+TonnyThe2nd@users.noreply.github.com>
2024-03-20 10:57:49 -0300

    Update AcessoOpcoes.java

--- AcessoOpcoes.java
+++ AcessoOpcoes.java
@@ -1,6 +1,6 @@
 public class AcessoOpcoes extends javax.swing.JFrame {
-    ClasseComandosUserUm cd = new ClasseComandosUserUm();
-    ClasseComandosUserDois cddois = new ClasseComandosUserDois();
+    ClasseComandosUserUm cd = new ClasseComandosUserUm("Antonio",123,0,1234,12345678);
+    ClasseComandosUserUm cddois = new ClasseComandosUserUm("Ana",123,1000,1234,12345678);
     String user;
     double main_saldo;
     double main_saldoDois = cddois.getSaldo();
